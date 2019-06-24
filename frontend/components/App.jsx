a47e1ec8ef1da0b90463326e08ca7fef8dab394f
christie <cbrandao18@gmail.com>
2019-06-24 17:27:36 -0400

    Style splash page

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -4,15 +4,16 @@ import { Route } from 'react-router-dom'
 import LoginFormContainer from '../components/session_form/login_form_container';
 import SignupFormContainer from '../components/session_form/signup_form_container';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import Splash from '../components/splash/splash';
 
 const App = () => (
     <div>
         <header>
-            <h1>Botify</h1>
-            <GreetingContainer />
+            
         </header>
         <AuthRoute exact path="/login" component={LoginFormContainer} />
         <AuthRoute exact path="/signup" component={SignupFormContainer} />
+        <Route exact path="/" component={Splash} />
     </div>
 );
 
---
+++ frontend/components/splash/splash.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class Splash extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return (
+            <div className="splash-main">
+                <div className="splash-top-nav">
+                    <div>
+                        <a href="#">
+                            <div className="white-logo"></div>
+                            <p>Botify</p>
+                        </a>
+                    </div>
+                    <div className="splash-nav-links">
+                        <a href="#/signup">Sign Up</a>
+                        <a href="#/login">Login</a>
+                    </div>
+                </div>
+
+                <div className="splash-text-content">
+                    <h1>Music for <p>everyone</p> robots.</h1>
+                    <h2>Millions of techno songs for the robots among us. No credit card needed.</h2>
+                    <a href="#/signup">GET BOTIFY FREE 🤖 </a>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Splash;
