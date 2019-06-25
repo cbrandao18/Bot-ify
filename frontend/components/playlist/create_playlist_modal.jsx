e1184d973b812565b34f1600ddefcad9e54a5f69
christie <cbrandao18@gmail.com>
2019-06-25 19:02:49 -0400

    Continue styling/creating new playlist form modal

--- frontend/components/playlist/create_playlist_modal.jsx
+++ frontend/components/playlist/create_playlist_modal.jsx
@@ -7,7 +7,18 @@ class CreatePlaylistModal extends React.Component{
         return (
             <div className="playlist-modal-wrapper">
                 <div className="playlist-form-wrapper">
-                    <Link className="playlist-close" to={'/browse/library'}>X</Link>
+                    <div className="playlist-close-wrapper">
+                        <Link className="playlist-close" to={'/browse/library'}>X</Link>
+                    </div>
+                    <h1>Create new playlist</h1>
+                    <div className="playlist-input-form-wrapper">
+                        <div className="input-box">
+                            <div className="content-spacing">
+                                <h4 class="inputBox-label">Playlist Name</h4>
+                                <input type="text" class="inputBox-input" placeholder="New Playlist" value=""></input>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
