ab6491a5a629ef3214b288d9f504437b66caebad
christie <cbrandao18@gmail.com>
2019-06-25 16:25:14 -0400

    Add nav links to side bar

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -5,10 +5,33 @@ const NavBar = ({ current_user, logout }) => {
     return (
         <div className="nav-bar">
             <div className="nav-bar-header">
-                
+                <div className="nav-bar-logo">
+                    <a href="#">
+                        <div className="white-logo"></div>
+                        <p>Botify</p>
+                    </a>
+                </div>
+
+                <div className="nav-bar-link">
+                    <i class="fas fa-home"></i>
+                    <span>Home</span>
+                </div>
+                <div className="nav-bar-link">
+                    <i class="fas fa-search"></i>
+                    <span>Search</span>
+                </div>
+                <div className="nav-bar-link">
+                    <i class="fas fa-music"></i>
+                    <span>Your Library</span>
+                </div>
+
             </div>
             <div className="nav-bar-footer">
-                <p className="session-user">{current_user.username}</p>
+                
+                <p className="session-user">
+                    <i className="fas fa-user"></i>
+                    {current_user.username}
+                </p>
                 <a 
                 className="green-button logout-nav" 
                 onClick={logout.bind(this)}>
