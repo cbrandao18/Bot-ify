6a8fbcc3f6e92e5fb3f743f6785727ed8ad00648
christie <cbrandao18@gmail.com>
2019-06-27 11:59:26 -0400

    Adjust default route when not logged in to be the splash page

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -16,9 +16,9 @@ const App = () => (
         {/* playbar the bottom */}
 
         <Switch>
+            <AuthRoute exact path="/" component={Splash} />
             <AuthRoute exact path="/login" component={LoginFormContainer} />
             <AuthRoute exact path="/signup" component={SignupFormContainer} />
-            <AuthRoute exact path="/" component={Splash} />
             <ProtectedRoute path="/browse/library/playlists" component={LibraryContainer} />
             <ProtectedRoute path="/browse/library/playlist/create" component={CreatePlaylistModalContainer} />
             <ProtectedRoute path="/playlists/:playlistId" component={PlaylistDetailContainer} />
