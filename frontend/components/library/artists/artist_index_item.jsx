6ea8c958de000d54004327a421089d020dfc49e0
christie <cbrandao18@gmail.com>
2019-06-28 18:27:26 -0400

    Make artists on index page clickable to the detail page

--- frontend/components/library/artists/artist_index_item.jsx
+++ frontend/components/library/artists/artist_index_item.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 class ArtistIndexItem extends React.Component {
     render(){
         return (
             <li className="artist-index-item">
-                <div className="artist-index-image"></div>
-                <p>{this.props.artist.name}</p>
+                <Link to={`/artists/${this.props.artist.id}`}>
+                    <div className="artist-index-image"></div>
+                    <p>{this.props.artist.name}</p>
+                </Link>
             </li>
         )
     }
