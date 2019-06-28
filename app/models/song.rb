6262684061886c20472f0013d37b22e16b91c1d6
christie <cbrandao18@gmail.com>
2019-06-28 11:36:44 -0400

    Add missing curly brace

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,4 @@
 class Song < ApplicationRecord
     validates :title, :length, :album_id, :explicit, :album_order, presence: true
-    validates :explicit, inclusion: {in: [true, false]
+    validates :explicit, inclusion: {in: [true, false]}
 end
