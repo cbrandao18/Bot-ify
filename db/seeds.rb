ab52b2a6b8269ad6f958dbd8e68d19fc3fbd1f50
christie <cbrandao18@gmail.com>
2019-07-01 15:25:22 -0400

    start seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# require 'open-uri'
+require 'open-uri'
 
 User.destroy_all
 Artist.destroy_all
@@ -14,20 +14,17 @@ Song.destroy_all
 
 robot = User.create(username: 'botuser', password: 'robots')
 
-artist1 = Artist.create(name: 'Post Malone', has_poster: true)
-artist2 = Artist.create(name: 'CRAY', has_poster: true)
-artist3 = Artist.create(name: 'ODESZA', has_poster: true)
+the_xx = Artist.create(name: 'The XX', has_poster: true)
+# odesza = Artist.create(name: 'ODESZA', has_poster: true)
 
-album1 = Album.create(title: 'Album1', artist_id: artist1.id)
-album2 = Album.create(title: 'Album2', artist_id: artist1.id)
-album3 = Album.create(title: 'Album3', artist_id: artist2.id)
+the_xx_artist_img = open('https://botify-dev.s3.us-east-2.amazonaws.com/artist_hero_images/the_xx.jpeg')
+the_xx.hero_image.attach(io: the_xx_artist_img, filename: 'the_xx.jpeg')
 
-album1_cover = open('https://botify-dev.s3.us-east-2.amazonaws.com/albums/1.jpg')
-album1.photo.attach(io: album1_cover, filename: '1.jpg')
+xx_album1 = Album.create(title: 'XX', artist_id: the_xx.id)
 
-Song.create(title: 'Album1Song1', length: 160, album_id: album1.id, explicit: true, album_order: 1)
-Song.create(title: 'Album1Song2', length: 140, album_id: album1.id, explicit: true, album_order: 2)
-Song.create(title: 'Album1Song3', length: 200, album_id: album1.id, explicit: true, album_order: 3)
+xx_album_cover = open('https://botify-dev.s3.us-east-2.amazonaws.com/albums/1.jpg')
+xx_album1.album_cover.attach(io: xx_album_cover, filename: '1.jpg')
 
-Song.create(title: 'Album2Song1', length: 160, album_id: album2.id, explicit: false, album_order: 1)
-Song.create(title: 'Album2Song2', length: 140, album_id: album2.id, explicit: true, album_order: 2)
+xx_album1_song1 = Song.create(title: 'Intro', length: 160, album_id: xx_album1.id, explicit: false, album_order: 1)
+xx_album1_song1_mp3 = open('https://botify-dev.s3.us-east-2.amazonaws.com/albums/TheXX-XX/01_Intro.mp3')
+xx_album1_song1.track.attach(io: xx_album1_song1_mp3, filename: '01_Intro.mp3')
